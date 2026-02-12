0d38fea53aa3309012e5ba95d86b38285b1ed6e4
JamesLouisCassells <w0517106@nscc.ca>
2026-02-12 09:30:37 -0400

    Fixed syntax issues breaking checkStraights

--- Assignment_2c/pokerHand.js
+++ Assignment_2c/pokerHand.js
@@ -171,34 +171,42 @@ function checkStraights(numberValues){
             name: "Straight",
             strength: 5,
             ranks: [high, high-1, high-2, high-3, high-4],
-            display: 'Straight (${high} high'
+            display: `Straight (${high} high)`
             };
     }
-   
     //solution for ace being 1 or 14 with low straights
     if (sorted.includes(14)) { //if the sorted array has an ace
         let aceLow = sorted.map(v => v === 14 ? 1 : v).sort((a, b) => a - b); //create a mew array and treat the ace as value 1 or 14 with ternary operator and check for 1-5
         consec = aceLow.filter((value, i) => aceLow[i + 1] === value + 1);
         if (consec.length === 4) {
-            const high = sorted[4];
             return {
                 name: "Straight",
                 strength: 5,
                 ranks: [5,4,3,2,1],
-                display: "Straight (${high} high"
+                display: "Straight 5 high"
                 }; //if yes then consider ace a 1 and return true
             }       
-    return false; //if no straights anywhere then return false
+    return null; //if no straights anywhere then return null (not false so it doesnt cause issues with evaluateHands)
     }
 }
 
 function checkRoyalFlush(checkFlush, numberValues){
     let sorted = [...numberValues].sort((a, b) => a - b); //creates a copy of the numberValues and sorts it by numeric value. Copy b/c sort mutates the original
     let consec = sorted.filter((value, i) => sorted[i + 1] === value + 1) //filter parses sorted, if the value at position 1 matches position 2 it adds them to a new array and checks the next value
-    if (sorted[0] === 10 && consec.length === 4 && checkFlush()) return true;
-    //explicitly check for royal flush but also flush
-    //return the cards and an award
+    if (sorted[0] === 10 && consec.length === 4 && checkFlush()) {
+        const high = sorted[4];
+        return {
+            name: "Royal Flush",
+                strength: 10,
+                ranks: [5,4,3,2,1],
+                display: "Royal Flush (${high} high"
+                };
+        }
 }
+
+
+
+
 document.getElementById("freshHand").addEventListener("click", getDeck);
 // #endregion
 
