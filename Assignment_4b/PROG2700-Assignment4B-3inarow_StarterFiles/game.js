d7f91dc07bcc253791cd443eecd0acbb5a544090
James Cassells <w0517106@nscc.ca>
2026-03-20 15:39:44 -0300

    Built click listeners to check the state of the data, update the data according to clicks then update the dom

--- Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
+++ Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
@@ -19,7 +19,7 @@
             console.log("return from querySelector: ", game_div);
             
             function createTable(p_data){                
-                game_div.innerHTML = ""; //clears the elements 
+                game_div.innerHTML = ""; //clears anything already insidet the game div before redrawing 
                 const table = document.createElement("table"); //creates the table
 
                 for (let row = 0; row < p_data.rows.length; row++) {
@@ -28,12 +28,38 @@
                         const tableData = document.createElement("td");
                         console.log(p_data.rows[row][col]); //Tells me the value of each cell (return is currentState, correctState, canToggle: true/false)
                         const cellInfo = p_data.rows[row][col]; //saving the value of each position in a vairable
+                       
+                        tableData.dataset.row = row; //saving the row data
+                        tableData.dataset.col = col;  //saving the col data
+                        
+                        //assigning colours based on api currentState value
                         if (cellInfo.currentState === 1) {
                             tableData.classList.add("blue-cell");
                             } 
                         else if (cellInfo.currentState === 2) {
                             tableData.classList.add("white-cell"); //or white
                             }
+                        //creating actions for clicking based on what values are stored in the api
+                        tableData.addEventListener("click", function () { 
+                            console.log("cell clicked");
+                            const clickRow = Number(this.dataset.row);
+                            const clickCol = Number(this.dataset.col);
+                            const clickCell = p_data.rows[clickRow][clickCol];
+                        //iif this is a fixed starting square, do nothing
+                        if (!clickCell.canToggle) {
+                            return;
+                        }
+                        //assigning state based on clicks
+                        clickCell.currentState = (clickCell.currentState + 1) % 3; //0 becomes 1, 1-2, 2-0 using remainder division
+                        this.classList.remove("blue-cell", "white-cell");
+
+                        if (clickCell.currentState === 1) {
+                            this.classList.add("blue-cell");
+                        }
+                        else if (clickCell.currentState === 2) {
+                            this.classList.add("white-cell");
+                        }
+                        });
                         
                         tableRow.appendChild(tableData); //populating the rows with the data
                     }
