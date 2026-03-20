efccc5619c0a6a767f23fc0b7e1137e785205eb6
James Cassells <w0517106@nscc.ca>
2026-03-20 14:03:06 -0300

    loop now assigns colours to the curentState passed in from the json

--- Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
+++ Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
@@ -1,9 +1,5 @@
 // Pseudo: 
-// 1) get anchor point (#theGame) DONE
-// 2) Create a table in memory
-// 3) Use data from json to build row and columns
-// - loop through 2d for rows and values
-// 4) connect elements with createElement and appendChild
+
 
 (() => { //wrapping my code IIFE
     const api_url = "https://prog2700.onrender.com/threeinarow/sample";
@@ -18,24 +14,36 @@
 
             const json = await response.json();
             console.log("json info received from api: ", json);
+
+            const game_div = document.querySelector("#theGame");//selects the entire page with document, QS chooses the element with id "#theGame" (no # would search for an element)
+            console.log("return from querySelector: ", game_div);
             
-            function createTable(p_data){
-                const game_div = document.querySelector("#theGame"); //selects the entire page with document, QS chooses the element with id "#theGame" (no # would search for an element)
-                console.log("return from querySelector: ", game_div);
-                game_div.innerHTML = ""; //c;ears the element
+            function createTable(p_data){                
+                game_div.innerHTML = ""; //clears the elements 
                 const table = document.createElement("table"); //creates the table
-                for (let row = 0; row < p_data.rows.length; row ++){ //length of the array row
+
+                for (let row = 0; row < p_data.rows.length; row++) {
                     const tableRow = document.createElement("tr");
-                    for(let col = 0; col < p_data.rows[row].length; col ++) {
+                    for (let col = 0; col < p_data.rows[row].length; col++) {
                         const tableData = document.createElement("td");
-                        tableData.appendChild(tableRow)
+                        console.log(p_data.rows[row][col]); //Tells me the value of each cell (return is currentState, correctState, canToggle: true/false)
+                        const cellInfo = p_data.rows[row][col]; //saving the value of each position in a vairable
+                        if (cellInfo.currentState === 1) {
+                            tableData.classList.add("blue-cell");
+                            } 
+                        else if (cellInfo.currentState === 2) {
+                            tableData.classList.add("white-cell"); //or white
+                            }
+                        
+                        tableRow.appendChild(tableData); //populating the rows with the data
                     }
-                    tableRow.appendChild(tableData);
-                }                
+                    table.appendChild(tableRow);//populating the table with the rows
+                }          
+                return table;      
             }
-            game_div.appendChild(createTable)
+            const builtTable = createTable(json); //calls the function with the json passed in
+            game_div.appendChild(builtTable);
         }
-            
 
         catch (error) {
                 console.error("Error fetching data:", error);
