463480eac663bd6da386af6d6c909e4079339727
James Cassells <w0517106@nscc.ca>
2026-03-20 08:50:54 -0300

    cleaning working tree

--- Assignment_3c/PROG2700-Assignment3-StarterCode-Flight/main.js
+++ Assignment_3c/PROG2700-Assignment3-StarterCode-Flight/main.js
@@ -1,4 +1,5 @@
 // IIFE
+console.log("SCRIPT LOADED ✅", new Date().toISOString());
 (() => {
     //creating icon variables ~~ mapping file numbers to a function with object of size, anchor, url thats tied to a variable
     const p_planeIcons = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29].map(p_num => 
