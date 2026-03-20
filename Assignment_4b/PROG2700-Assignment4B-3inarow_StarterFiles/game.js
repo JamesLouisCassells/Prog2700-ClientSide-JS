4b9716458fafe9080da0a5fd3c6ab4b79d860fd8
James Cassells <w0517106@nscc.ca>
2026-03-20 11:17:24 -0300

    Iterated through building table, row, row data. Now to build a loop based off of the json information

--- Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
+++ Assignment_4b/PROG2700-Assignment4B-3inarow_StarterFiles/game.js
@@ -1,5 +1,5 @@
 // Pseudo: 
-// 1) get anchor point (#theGame)
+// 1) get anchor point (#theGame) DONE
 // 2) Create a table in memory
 // 3) Use data from json to build row and columns
 // - loop through 2d for rows and values
@@ -18,8 +18,18 @@
 
             const json = await response.json();
             console.log("json info received from api: ", json);
-            const game_div = document.querySelector("#theGame"); //selects the entire page with document, QS chooses the element with id "theGame"
+            //link the dom to the div
+            const game_div = document.querySelector("#theGame"); //selects the entire page with document, QS chooses the element with id "#theGame" (no # would search for an element)
             console.log("return from querySelector: ", game_div);
+            //create a table to put in the div
+            const table = document.createElement("table");
+            //attach that table to the linked div
+            game_div.appendChild(table);
+            //creating a row and adding it to the table
+            var row = document.createElement("tr");
+            table.appendChild(row);
+            var data = document.createElement("td");
+            row.appendChild(data);
         }
 
         catch (error) {
