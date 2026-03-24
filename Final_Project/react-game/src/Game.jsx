2e946430cb0e142894b6784205feb8786c99e9a3
James Cassells <w0517106@nscc.ca>
2026-03-24 16:40:23 -0300

    working on react game

--- Final_Project/react-game/src/Game.jsx
+++ Final_Project/react-game/src/Game.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import Grid from "./Grid";
-
+//this is my most important file that uses the most react actions
 function Game() {
-    const p_size = 6;
+    const p_size = 6; //fixing the size as six for my grid
 
-    const [p_grid, setGrid] = useState(
+    const [p_grid, setGrid] = useState( //p_grid takes the paramter for the grid size, setGrid updates it. useState builds my grid
         Array(p_size).fill(null).map(() => Array(p_size).fill(""))
     );
 
-    function handleCellClick(p_row, p_col) {
-        const newGrid = p_grid.map((row) => [...row]);
-        newGrid[p_row][p_col] = newGrid[p_row][p_col] === "" ? "X" : "";
-        setGrid(newGrid);
+    //function to handle clicks
+    function handleCellClick(p_row, p_col) { //takes the specific row and column as parameters that the user clicked
+        const newGrid = p_grid.map((row) => [...row]); //maps a new grid instead of mutating original
+        newGrid[p_row][p_col] = newGrid[p_row][p_col] === "" ? "X" : ""; //empty cell gets an x, otherwise clear it (ON THE NEW GRID)
+        setGrid(newGrid); //triggers a new grid update (a react feature!)
     }
 
     return <Grid p_grid={p_grid} p_onClick={handleCellClick} />;
