bb16cd2bda101841a38fb5335fd19076ca159614
James Cassells <w0517106@nscc.ca>
2026-03-24 17:31:45 -0300

    added a reset button for tidiness

--- Final_Project/react-game/src/Game.jsx
+++ Final_Project/react-game/src/Game.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Grid from "./Grid";
+//where my grid lives and updates
 //this is my most important file that uses the most react actions
 function Game() {
     const p_size = 6; //fixing the size as six for my grid
@@ -8,7 +9,7 @@ function Game() {
         Array(p_size).fill(null).map(() => Array(p_size).fill("")) //Creates a grid with empty strings. Fill() makes it useable
     );
 
-    //function to handle clicks
+    //function to handle clicks - wrapped in a function so it doesnt load on page load but on click
     function handleCellClick(p_row, p_col) { //takes the specific row and column as parameters that the user clicked
         const newGrid = p_grid.map((row) => [...row]); //maps a new grid instead of mutating original
         newGrid[p_row][p_col] = newGrid[p_row][p_col] === "" ? "X" : ""; //empty cell gets an x, otherwise clear it (ON THE NEW GRID)
@@ -17,5 +18,15 @@ function Game() {
 
     return <Grid p_grid={p_grid} p_onClick={handleCellClick} />;
 }
+
+function resetGame() {
+    const emptyGrid = Array(p_size)
+        .fill(null)
+        .map(() => Array(p_size).fill(""));
+
+    setGrid(emptyGrid);
+}
+
+<button onClick={resetGame}>Reset</button>
 
 export default Game;
